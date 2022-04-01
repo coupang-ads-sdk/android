be9446ba747a3ada65c8846a4df9ba21bacd5c0c
gusun1 <gusun1@conpang.com>
2022-04-01 11:00:21 +0800

    ads banner demo

---
+++ com/coupang/examples/1.0.2/BannerAds/app/src/main/java/com/coupang/ads/demo/BannerAdsApplication.kt
@@ -0,0 +1,15 @@
+package com.coupang.ads.demo
+
+import android.app.Application
+import com.coupang.ads.CoupangAds
+
+class BannerAdsApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        //Use your own affiliate id, sub id, and widget id.
+        CoupangAds.init(context = this, "AFSDKDEMO", "BannarAdsDemo")
+    }
+
+}
