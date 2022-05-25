417318693f4d99c3b3bb0181297eaa2942d1a4f1
gusun1 <gusun1@conpang.com>
2022-05-25 09:45:24 +0800

    remove useless files & add observer for bannerViewModel

--- com/coupang/examples/1.1.2/BannerAds/app/src/main/java/com/coupang/ads/bannerads/MainActivity.kt
+++ com/coupang/examples/1.1.2/BannerAds/app/src/main/java/com/coupang/ads/bannerads/MainActivity.kt
@@ -2,6 +2,8 @@ package com.coupang.ads.bannerads
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
 import com.coupang.ads.config.AdsCreativeSize
 import com.coupang.ads.config.AdsMode
 import com.coupang.ads.tools.adsViewModels
@@ -17,6 +19,16 @@ class MainActivity : AppCompatActivity() {
         "Bottom Banner"  // optional, location of the ad.
     )
 
+    private val bannerDataObserver by lazy {
+        Observer<Result<*>> {
+            if (it.isSuccess) {
+                Log.i("bannerObserver", "banner ads download success")
+            } else {
+                Log.i("bannerObserver", "banner ads download failed", it.exceptionOrNull())
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +37,7 @@ class MainActivity : AppCompatActivity() {
                 this,
                 bannerViewModel
             )
+            bannerViewModel.observe(this, bannerDataObserver)
             bannerViewModel.loadAdData()
         }
     }
