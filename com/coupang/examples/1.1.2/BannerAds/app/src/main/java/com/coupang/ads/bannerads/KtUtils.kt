be4978ed655854044a14f6d5eaae1b8293e27380
gusun1 <gusun1@conpang.com>
2022-05-27 10:12:31 +0800

    remove java examples

--- com/coupang/examples/1.1.2/BannerAds/app/src/main/java/com/coupang/ads/bannerads/KtUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.coupang.ads.bannerads
-
-import androidx.lifecycle.LiveData
-
-fun <T> LiveData<Result<T>>.getResult(): com.coupang.ads.java.Result<T> {
-    return com.coupang.ads.java.Result(this.value!!)
-}
--- com/coupang/examples/1.1.2/InterstitialAds/app/src/main/java/com/coupang/ads/example/interstitial/KtUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.coupang.ads.example.interstitial
-
-import androidx.lifecycle.LiveData
-
-fun <T> LiveData<Result<T>>.getResult(): com.coupang.ads.java.Result<T> {
-    return com.coupang.ads.java.Result(this.value!!)
-}
